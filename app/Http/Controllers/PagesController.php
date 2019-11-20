c1c5d004c034a548dc968aad700494ce958f1a2e
stevekama <stevekamahertz@gmail.com>
2019-11-20 07:20:57 +0300

    my index page

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    // index page 
+    public function index(){
+        return view('pages.index');
+    }
+    
+}
